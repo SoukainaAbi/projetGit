3297cfda0fc400d332c1bdb97cb88a6ab9307b9e
SoukainaAbi <abidsoukaina9@gmail.com>
2017-10-04 02:59:20 +0100

    mod Sxx

--- helloWorld/src/Sxx.java
+++ helloWorld/src/Sxx.java
@@ -3,7 +3,9 @@ public class Sxx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
