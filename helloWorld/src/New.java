bdf59fba5ab767c41eca5f51d09ec39d607511e0
SoukainaAbi <abidsoukaina9@gmail.com>
2017-10-04 02:48:33 +0100

    modf classes

---
+++ helloWorld/src/New.java
@@ -0,0 +1,12 @@
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+	}
+
+}
---
+++ helloWorld/src/Sxx.java
@@ -0,0 +1,9 @@
+
+public class Sxx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
